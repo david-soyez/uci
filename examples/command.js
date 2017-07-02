7a2913e2b4e105cd19c170a97fbc98753bd6c361
David Soyez <david.soyez@interadcorp.com>
2017-07-02 17:54:04 -0400

    change command sample

--- examples/command.js
+++ examples/command.js
@@ -1,10 +1,9 @@
 var Engine = require('../src/main.js');
 var engine = new Engine('stockfish');
 engine.runProcess().then(function () {
-    console.log('Started');
-    return engine.uciCommand();
-}).then(function (idAndOptions) {
-    console.log('Engine name - ' + idAndOptions.id.name);
+    return engine.command('uci');
+}).then(function (data) {
+    console.log(data);
     return engine.command('isready');
 }).then(function (data) {
     console.log(data);
